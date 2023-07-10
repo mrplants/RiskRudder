611573bc29cafefb624b9246e507dd5bb6f6bd0c
Sean F <fitzgerald.sean.t@gmail.com>
2023-07-09 20:21:53 -0700

    Initial Commit

---
+++ RiskRudder/RiskRudderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiskRudderApp.swift
+//  RiskRudder
+//
+//  Created by Sean Fitzgerald on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct RiskRudderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
